b276c4382901c4049a2ae712ccc683395e90c7b0
Daniyal Ahmed <dahmed@citygovapp.com>
2024-10-09 01:41:06 -0700

    Initial Commit

---
+++ Universities/UniversitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversitiesApp.swift
+//  Universities
+//
+//  Created by Daniyal Ahmed on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct UniversitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
